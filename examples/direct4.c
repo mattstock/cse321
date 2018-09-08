44654c89373fcbb27769ddbf7075b9789061f1f7
Matt Stock <stock@bexkat.com>
2018-09-08 19:15:09 -0400

    clearer example

--- examples/direct4.c
+++ examples/direct4.c
@@ -3,16 +3,10 @@
 uint8_t * const registers = (uint8_t *)0x20;
 
 #define PORTB (registers[0x18])
-#define DDRB (registers[0x17])
-#define PINB (registers[0x16])
 
 int main() {
-  DDRB = 1; // Pin 0 is output, rest are inputs
-  while (1) {
-    if (PINB & 0b10000) {
-      PORTB &= ~1;
-    } else {
-      PORTB |= 1;
-    }
+  PORTB = 0x79;
+  if (PORTB) {
+    asm("nop");
   }
 }
